207060cb02ac1cef299a0a50390a14d2a0fc6d88
Scotty Jacobson <ScottyMJacobson@gmail.com>
2021-10-06 16:02:46 -0400

    Add <title> to ScottyMJacobson.com CU-1jv4ep0

--- pages/index.page.js
+++ pages/index.page.js
@@ -1,9 +1,14 @@
 import App from './App'
 import { ThemeProvider } from './contexts/theme'
+import Head from 'next/head'
 
 export default function Home() {
   return (
   <ThemeProvider>
+    <Head>
+        <title>Scotty Jacobson - Full Stack Engineer</title>
+        <meta property="og:title" content="Scotty Jacobson - Full Stack Engineer" key="title" />
+      </Head>
     <App />
   </ThemeProvider>
   );
